b9abb3abaa848aeac3ddd8799e5b6462256fc171
samruddhisomani <sammysamhy@gmail.com>
2016-03-26 15:51:39 -0500

    Updates

--- recipeminder/settings.py
+++ recipeminder/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'recipes',
+    'bootstrap3',
 ]
 
 MIDDLEWARE_CLASSES = [
@@ -121,3 +122,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+BOOTSTRAP3 = {'theme_url': 'https://bootswatch.com/united/bootstrap.min.css'}
